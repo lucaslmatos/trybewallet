5bc46361832a13a596346adf2a694bc0d2e61624
Lucas Matos <lucaslmatos18@gmail.com>
2023-03-13 17:58:24 -0300

    Implementado itens á tabela vindos do estago global. REQ 07

--- src/components/Table.js
+++ src/components/Table.js
@@ -1,23 +1,55 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
 
 class Table extends Component {
   render() {
+    const { expenses } = this.props;
     return (
       <table>
-        <tr>
-          <th>Descrição</th>
-          <th>Tag</th>
-          <th>Método de pagamento</th>
-          <th>Valor</th>
-          <th>Moeda</th>
-          <th>Câmbio utilizado</th>
-          <th>Valor convertido</th>
-          <th>Moeda de conversão</th>
-          <th>Editar/Excluir</th>
-        </tr>
+        <thead>
+          <tr>
+            <th>Descrição</th>
+            <th>Tag</th>
+            <th>Método de pagamento</th>
+            <th>Valor</th>
+            <th>Moeda</th>
+            <th>Câmbio utilizado</th>
+            <th>Valor convertido</th>
+            <th>Moeda de conversão</th>
+            <th>Editar/Excluir</th>
+          </tr>
+        </thead>
+        <tbody>
+          {expenses.map((exp, index) => (
+            <tr key={ index }>
+              <td>{ exp.description }</td>
+              <td>{ exp.tag }</td>
+              <td>{ exp.method }</td>
+              <td>{ parseFloat(exp.value).toFixed(2) }</td>
+              <td>{ exp.currency }</td>
+              <td>BRL</td>
+              <td>
+                {
+                  parseFloat(exp.value * exp.exchangeRates[exp.currency].ask).toFixed(2)
+                }
+                /
+                {
+                  parseFloat(exp.exchangeRates[exp.currency].ask).toFixed(2)
+                }
+              </td>
+              <td>{ exp.exchangeRates[exp.currency].name }</td>
+            </tr>
+          ))}
+        </tbody>
       </table>
     );
   }
 }
 
-export default Table;
+const mapStateToProps = (state) => ({
+  expenses: state.wallet.expenses,
+});
+
+Table.propTypes = {}.isRequired;
+
+export default connect(mapStateToProps)(Table);
